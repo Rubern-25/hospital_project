7f054429355ae1314e1b775219e1f1a25af79069
Rubern-25 <rubernmwanakatwe97@gmail.com>
2026-02-26 18:36:41 +0300

    Fix indentation in deployment.py

--- django_backend/hpms_project/deployment.py
+++ django_backend/hpms_project/deployment.py
@@ -32,7 +32,8 @@ STORAGES = {
 CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 CONNECTION_STR = {pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split(' ')}
 DATABASES = {
-    "default": {"NAME": CONNECTION_STR['dbname'],
+        "default": {
+        "NAME": CONNECTION_STR['dbname'],
         "HOST": CONNECTION_STR['host'],
         "USER": CONNECTION_STR['user'],
         "PASSWORD": CONNECTION_STR['password'],           # or IP/hostname
