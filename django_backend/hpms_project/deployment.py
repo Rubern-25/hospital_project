2572dcad7e4310a21e309c0e92a5250624a1c1e6
Rubern-25 <rubernmwanakatwe97@gmail.com>
2026-02-26 17:47:01 +0300

    Remove CORS_ALLOWED_ORIGINS from deployment settings
    Removed CORS_ALLOWED_ORIGINS configuration.

--- django_backend/hpms_project/deployment.py
+++ django_backend/hpms_project/deployment.py
@@ -16,10 +16,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-]
+
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 STORAGES = {
     "default": {
