310450dad15d6e1eca78e12fd06fec2d8273bb86
Rubern-25 <rubernmwanakatwe97@gmail.com>
2026-02-27 17:46:20 +0300

    Update settings.py

--- django_backend/hpms_project/settings.py
+++ django_backend/hpms_project/settings.py
@@ -106,6 +106,8 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://brave-water-032ac4a03.6.azurestaticapps.net',
+
 ]
 
 # Django REST Framework
