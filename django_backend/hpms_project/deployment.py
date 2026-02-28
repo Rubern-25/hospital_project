b7b4eb7404ea9f5d83a9210a8b444fc90f81bcb2
Rubern-25 <rubernmwanakatwe97@gmail.com>
2026-02-28 08:23:55 +0300

    GOD HELP ME

--- django_backend/hpms_project/deployment.py
+++ django_backend/hpms_project/deployment.py
@@ -2,7 +2,10 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://'+os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = [
+    'https://' + os.environ['WEBSITE_HOSTNAME'],
+    'https://brave-water-032ac4a03.6.azurestaticapps.net',
+]
 DEBUG = False
 
 MIDDLEWARE = [
@@ -26,8 +29,13 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
 },}
 CORS_ALLOWED_ORIGINS = [
-    'https://brave-water-032ac4a03.6.azurestaticapps.net/' 
+    'https://brave-water-032ac4a03.6.azurestaticapps.net',
 ]
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 CONNECTION_STR = {pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split(' ')}
 DATABASES = {
