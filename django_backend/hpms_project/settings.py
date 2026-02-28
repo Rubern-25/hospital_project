1a5f161421e137fb503e780244fb57783528707e
Rubern-25 <rubernmwanakatwe97@gmail.com>
2026-02-28 15:19:42 +0300

    Update settings.py

--- django_backend/hpms_project/settings.py
+++ django_backend/hpms_project/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "django-backend-database",                        # the database you just created
+        "NAME": "hospital_db",                        # the database you just created
         "USER": "edrvdmegie@djando-backend-server",   # full username from Azure
         "PASSWORD": "123",          # the password you set
         "HOST": "djando-backend-server.postgres.database.azure.com",
